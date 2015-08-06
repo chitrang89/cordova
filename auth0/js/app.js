08c54af45e6e06da99d60684ea6964c8f3dabf1c
chitrang thakkar <chitrang89@live.com>
2015-08-06 03:13:15 +0000

    ->added location/internet connection check capability ->added option to view/hide login screen ->added ngCordova (AngularJS for Cordova) and ngStorage helper libraries

--- auth0/js/app.js
+++ auth0/js/app.js
@@ -5,33 +5,60 @@ document.addEventListener('deviceready',function() {
       AUTH0_DOMAIN
     );
 
+    document.getElementById('login').style.display='none';
     var userProfile;
+   function checkConnection() {
+            return navigator.connection.type;
+            
+        }
+    
+    function checkGPS() {
+        var position =  navigator.geolocation.getCurrentPosition(onSuccess,onError);
+        function onError(){
+            
+        }
+        function onSuccess(position) {
+            
+            var str = 'Latitude: '+ position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n';
+          $('#display-stuff').text(str);  
+        };
+    
+    }
+    
+    function clearDisplayStuff() {
+        $('#display-stuff').text('');
+    }
 
     $('.btn-login').click(function(e) {
       e.preventDefault();
       lock.show(function(err, profile, token) {
         if (err) {
           // Error callback
-          console.log("There was an error");
-          alert("There was an error logging in");
+          console.log('There was an error');
+          alert('There was an error logging in');
         } else {
           // Success calback
 
           // Save the JWT token.
           localStorage.setItem('userToken', token);
             
-            /*var response = $.ajax({
+          
+            
+            var response = $.ajax({
                 url: 'http://testczapi.azurewebsites.net/api/secured/ping',
                 method: 'GET',
                 success: function(resp) {
                     $('#display-stuff').text(resp);
                 }
-            });*/
-            
-            
-            
-            
-            
+            });
+           
           // Save the profile
           userProfile = profile;
 
@@ -53,6 +80,24 @@ document.addEventListener('deviceready',function() {
       }
     });
 
+    
+    
+    $('.btn-connectionType').click(function(e){
+        
+       var response= checkConnection();
+        $('#display-stuff').text('Connected via:'+response);
+    })
+    
+    $('.btn-gps').click(function(e){
+        
+       checkGPS();
+            })
+    
+    $('.btn-clear').click(function(e){
+        
+       clearDisplayStuff();
+    })
+    
     $('.btn-auth').click(function(e) {
       // Just call your API here. The header will be sent
       $.ajax({
@@ -60,8 +105,9 @@ document.addEventListener('deviceready',function() {
         method: 'GET'
       }).then(function(data, textStatus, jqXHR) {
         $('#display-stuff').text(data);
+          
       }, function() {
-        alert("You need to download the server seed and start it to call this API");
+        alert('You need to download the server seed and start it to call this API');
       });
     })
     
@@ -72,8 +118,10 @@ document.addEventListener('deviceready',function() {
         method: 'GET'
       }).then(function(data, textStatus, jqXHR) {
         $('#display-stuff').text(data);
+         
+          
       }, function() {
-        alert("You need to download the server seed and start it to call this API");
+        alert('You need to download the server seed and start it to call this API');
       });
     })
 
